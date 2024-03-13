94b86bc73930b5e14f8f042fc1fb1c0321896e4e
Bonekazz <juracirdantas@gmail.com>
2024-03-12 23:50:27 -0300

    code refactored

--- src/main.ts
+++ src/main.ts
@@ -1,65 +1,83 @@
-//import { createIcons, Plus, Minus} from 'lucide';
+import { createIcons, Plus, Minus, RotateCcw, Eye} from 'lucide';
 
 // ICONS 
-//createIcons({
-//  icons: {
-//    Plus,
-//    Minus
-//  }
-//})
-
-let leftSidePoint = 2;
-let rightSidePoint = 9;
-
-const leftSideElement = document.querySelector("#leftside");
-const rightSideElement = document.querySelector("#rightside");
-const resetElement = document.querySelector("#reset-btn");
-
-const addLeft = document.querySelector("#add-left-btn");
-const subLeft = document.querySelector("#sub-left-btn");
-const addRight = document.querySelector("#add-right-btn");
-const subRight = document.querySelector("#sub-right-btn");
-
-addLeft?.addEventListener("click", () => {
-  if (leftSideElement === null) throw new Error("elemento com id 'leftside' nao encontrado");
-  leftSidePoint += 1;
-  leftSideElement.innerHTML = String(leftSidePoint);
-  console.log("add left");
-});
-subLeft?.addEventListener("click", () => {
-  if (leftSideElement === null) throw new Error("elemento com id 'leftside' nao encontrado");
-  if (leftSidePoint === 0) return;
-
-  leftSidePoint -= 1;
-  leftSideElement.innerHTML = String(leftSidePoint);
-  console.log("sub left");
-});
-addRight?.addEventListener("click", () => {
-  if (rightSideElement === null) throw new Error("elemento com id 'rightside' nao encontrado");
-  rightSidePoint += 1;
-  rightSideElement.innerHTML = String(rightSidePoint);
-  console.log("add right");
-});
-subRight?.addEventListener("click", () => {
-  if (rightSideElement === null) throw new Error("elemento com id 'rightside' nao encontrado");
-  if (rightSidePoint === 0) return;
-  rightSidePoint -= 1;
-  rightSideElement.innerHTML = String(rightSidePoint);
-  console.log("sub right");
-});
-
-resetElement?.addEventListener("click", () => {
-  if (leftSideElement === null) throw new Error("elemento com id 'leftside' nao encontrado");
-  if (rightSideElement === null) throw new Error("elemento com id 'rightside' nao encontrado");
-
-  leftSidePoint = 0;
-  rightSidePoint = 0;
-  leftSideElement.innerHTML = String(leftSidePoint);
-  rightSideElement.innerHTML = String(rightSidePoint);
+createIcons({
+  icons: {
+    Plus,
+    Minus,
+    RotateCcw,
+    Eye
+  }
 })
+
+let leftSidePoint = 0;
+let rightSidePoint = 0;
+
+const leftSideElement = document.getElementById("leftside");
+const rightSideElement = document.getElementById("rightside");
 
 if (leftSideElement === null) throw new Error("elemento com id 'leftside' nao encontrado");
 if (rightSideElement === null) throw new Error("elemento com id 'rightside' nao encontrado");
 
 leftSideElement.innerHTML = String(leftSidePoint);
 rightSideElement.innerHTML = String(rightSidePoint);
+
+
+const actionBtn = document.querySelectorAll(".action-btn");
+
+actionBtn.forEach(button => {
+  button.addEventListener("click", (event) => {
+
+    switch ((event.currentTarget as HTMLButtonElement).id) {
+      case "add-left-btn":
+        if (leftSideElement === null) throw new Error("elemento com id 'leftside' nao encontrado");
+        leftSidePoint += 1;
+        leftSideElement.innerHTML = String(leftSidePoint);
+        break;
+      
+      case "sub-left-btn":
+        if (leftSideElement === null) throw new Error("elemento com id 'leftside' nao encontrado");
+        if (leftSidePoint === 0) return;
+
+        leftSidePoint -= 1;
+        leftSideElement.innerHTML = String(leftSidePoint);
+        break;
+      
+      case "add-right-btn":
+        if (rightSideElement === null) throw new Error("elemento com id 'rightside' nao encontrado");
+        rightSidePoint += 1;
+        rightSideElement.innerHTML = String(rightSidePoint);
+        break;
+      
+      case "sub-right-btn":
+        if (rightSideElement === null) throw new Error("elemento com id 'rightside' nao encontrado");
+        if (rightSidePoint === 0) return;
+        rightSidePoint -= 1;
+        rightSideElement.innerHTML = String(rightSidePoint);
+        break;
+      
+      case "reset-btn":
+        if (leftSideElement === null) throw new Error("elemento com id 'leftside' nao encontrado");
+        if (rightSideElement === null) throw new Error("elemento com id 'rightside' nao encontrado");
+
+        leftSidePoint = 0;
+        rightSidePoint = 0;
+        leftSideElement.innerHTML = String(leftSidePoint);
+        rightSideElement.innerHTML = String(rightSidePoint);
+        break;
+    
+      default:
+        console.log("somethin went wrong");
+        break;
+    }
+
+    button.addEventListener("touchstart", (event) => {
+      (event.currentTarget as HTMLButtonElement).style.transform = "scale(0.80)";
+    })
+    button.addEventListener("touchend", (event) => {
+      (event.currentTarget as HTMLButtonElement).style.transform = "scale(1)";
+    })
+    
+    
+  });
+});
